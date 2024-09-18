77994b0e2265d271d11030b7b892822a78e4653f
gary zhu <garyzhu@garys-MacBook-Air.fritz.box>
2024-09-18 22:02:05 +1200

    adds brew formula

---
+++ crgenerator.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Crgenerator < Formula
+    desc "Analysis Git commits, extract JIRA issues and pull details for Change Request process"
+    homepage "https://github.com/GaaraZhu/crgenerator"
+    url "https://github.com/GaaraZhu/crgenerator/releases/download/v0.1.0/crgenerator"
+    sha256 "24147499d7ca9b15ed378619b1bfc7552d9b337ba0801cd1c32204c6fb624604"
+    license "MIT"
+
+    # depends_on "cmake" => :build
+    def install
+        bin.install "crgenerator"
+    end
+
+end
