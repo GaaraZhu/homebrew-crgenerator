b083a2e2b952c5a51876c71f69e952e2e52aa3ec
gary zhu <garyzhu@garys-Air.fritz.box>
2024-09-21 12:44:22 +1200

    bump up to v0.1.4

--- crgenerator.rb
+++ crgenerator.rb
@@ -4,8 +4,8 @@
 class Crgenerator < Formula
     desc "Analysis Git commits, extract JIRA issues and pull details for Change Request process"
     homepage "https://github.com/GaaraZhu/crgenerator"
-    url "https://github.com/GaaraZhu/crgenerator/releases/download/v0.1.3/crgenerator"
-    sha256 "f5fe8f6cc146e63734ee94b636576f9702ca7d532c10aaf388ccaabc291f9b27"
+    url "https://github.com/GaaraZhu/crgenerator/releases/download/v0.1.4/crgenerator"
+    sha256 "307fbf696cce56a47f614f8398570ee636d04111496afe0270ab09e21f2a83b3"
     license "MIT"
 
     # depends_on "cmake" => :build
